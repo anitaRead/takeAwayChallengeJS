b4ec468d3d278473330ed114088ff8b88edd22d5
anitaRead <areadman91@gmail.com>
2021-07-09 11:11:48 +0100

    Selects a number of available dishes: #2

--- spec/takeawaySpec.js
+++ spec/takeawaySpec.js
@@ -6,6 +6,14 @@ describe("Takeaway", () => {
     });
 
     it("shows a list of dishes with prices", () => {
-        expect(takeaway.displayMenu()).toEqual([["Chips", "£2.00"], ["Beef Burger", "£12.00"]]);
+        expect(takeaway.displayMenu()).toEqual([["Chips", 2], 
+        ["Beef Burger", 12]]);
     });
+
+    it("selects a number of available dishes", () => {
+        takeaway.selectDish("Chips", 3);
+        takeaway.selectDish("Beef Burger", 12);
+        expect(takeaway.displayOrder()).toEqual([["Chips", 3], ["Beef Burger", 12]]);
+    });
+    
 });
--- src/takeaway.js
+++ src/takeaway.js
@@ -1,10 +1,23 @@
 class Takeaway {
 
     constructor() {
-        this.menu = [["Chips", "£2.00"], ["Beef Burger", "£12.00"]];
-    }
+        this.menu = [["Chips", 2], ["Beef Burger", 12]];
+        this.order = [];
+    };
 
     displayMenu() {
         return this.menu;
-    }
+    };
+
+    selectDish(dish, quantity) {
+        for(let i=0; i<this.menu.length; i++) {
+            if(this.menu[i][0] === dish) {
+                return this.order.push([dish, quantity]);
+            };
+        };
+    };
+
+    displayOrder() {
+        return this.order;
+    };
 }
